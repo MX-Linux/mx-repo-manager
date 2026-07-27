42243c94e87ef0ed28f746c3ac838a8f2d203d06
Adrian <adrian@mxlinux.org>
2026-07-26 23:15:06 -0400

    Update copyright headers

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # **********************************************************************
-# * Copyright (C) 2016-2025 MX Authors
+# * Copyright (C) 2016-2026 MX Authors
 # *
 # * Authors: Adrian
 # *          MX Linux <http://mxlinux.org>
